7e14db650b791d7d4c0969eaebb9ef3a9798beec
vireshh22 <viresh0489@gmail.com>
2024-10-06 21:50:50 -0400

    implemented rowMapper

--- src/main/java/com/springJDBC/App.java
+++ src/main/java/com/springJDBC/App.java
@@ -1,8 +1,11 @@
 package com.springJDBC;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class App 
 {
@@ -16,9 +19,8 @@ public class App
         // inserting data:
         String insert_query = "insert into employee(e_id,e_name,e_contact) values(?,?,?)";
         //firing the query
-        int rows = jdbcTemplate.update(insert_query,11,"ABC","1234567890");
+        int rows = jdbcTemplate.update(insert_query,20,"ABC","1234567890");
         System.out.println("Inserted: " + rows);
-        
         
         // updating data
         String update_query = "update employee set e_contact=? where e_contact='7558570489'";
@@ -29,5 +31,11 @@ public class App
         String delete_query = "delete from employee where e_name=?";
         int deleted_rows = jdbcTemplate.update(delete_query,"ABC");
         System.out.println("Deleted Rows: " + deleted_rows);
+        
+        // fetching data(select query)
+        String select_query = "select * from employee where e_id=?";
+        RowMapper<Employee> rm = new RowMapperImpl();
+        Employee emp = jdbcTemplate.queryForObject(select_query, rm,1);
+        System.out.println(emp);
     }
 }
---
+++ src/main/java/com/springJDBC/RowMapperImpl.java
@@ -0,0 +1,21 @@
+package com.springJDBC;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.lang.Nullable;
+
+public class RowMapperImpl implements RowMapper<Employee>{
+
+	@Override
+	@Nullable
+	public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Employee emp = new Employee();
+		emp.setE_id(rs.getInt(1));
+		emp.setE_name(rs.getString(2));
+		emp.setE_contact(rs.getString(3));
+		return emp;
+	}
+
+}
